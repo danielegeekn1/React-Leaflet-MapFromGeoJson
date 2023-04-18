c4c291283c912a82358bda112e284dccf5b88582
Daniele Basile <daniele.basile.job@gmail.com>
2023-04-18 17:28:43 +0200

    added import of geojson in myapp component

--- src/components/MyLMap.jsx
+++ src/components/MyLMap.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+import { Map, GeoJSON } from "react-leaflet";
 import countries from "./../data/countries.json";
+import "leaflet/dist/leaflet.css";
 class MyLMap extends Component {
   state = {};
   componentDidMount() {
@@ -9,6 +11,9 @@ class MyLMap extends Component {
     return (
       <div>
         <h1 style={{ textAlign: "center" }}>My leaflet-map </h1>
+        <Map style={{ height: "80vh" }} zoom={2} center={[20, 100]}>
+          <GeoJSON data={countries.feature} />
+        </Map>
       </div>
     );
   }
