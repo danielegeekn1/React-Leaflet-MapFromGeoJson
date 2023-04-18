218a32bf94598328a6bfea62a2343e31772e512f
Daniele Basile <daniele.basile.job@gmail.com>
2023-04-18 17:31:55 +0200

    added map from geojson features

--- src/components/MyLMap.jsx
+++ src/components/MyLMap.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Map, GeoJSON } from "react-leaflet";
+import { MapContainer, GeoJSON } from "react-leaflet";
 import countries from "./../data/countries.json";
 import "leaflet/dist/leaflet.css";
 class MyLMap extends Component {
@@ -11,9 +11,9 @@ class MyLMap extends Component {
     return (
       <div>
         <h1 style={{ textAlign: "center" }}>My leaflet-map </h1>
-        <Map style={{ height: "80vh" }} zoom={2} center={[20, 100]}>
-          <GeoJSON data={countries.feature} />
-        </Map>
+        <MapContainer style={{ height: "80vh" }} zoom={2} center={[20, 100]}>
+          <GeoJSON data={countries.features} />
+        </MapContainer>
       </div>
     );
   }
